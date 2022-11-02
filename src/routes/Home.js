47037590c3d42094220c70c895816640b4322aa6
mykim929 <mykim929@nexon.co.kr>
2022-11-02 16:22:21 +0900

    저장 기능 제작중1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Home = () => {
+    // const [viewList, setViewList] = useState("");
+    // const onSubmit = (event) => {
+    //     event.preventDefault();
+    // }
     return <span>Home</span>;
 }
 
--- src/routes/Write.js
+++ src/routes/Write.js
@@ -1,5 +1,20 @@
 const Write = () => {
-    return <span>Write</span>;
+    const [review, setReview] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {
+            target: {value}
+        } = event;
+        setReview(value);
+    }
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" value={review} onChange={onChange} placeholder="1" maxLenght={1000} />
+            <input type="submit" value="review" />
+        </form>
+    );
 }
 
 export default Write;
