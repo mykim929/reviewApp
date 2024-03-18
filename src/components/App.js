e47221992c8589b4ea6464379c0e018fc9d1c789
mykim929 <mykim929@nexon.co.kr>
2024-03-18 17:56:19 +0900

    로딩모션 추가

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,30 @@ function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	useEffect(() => {
 		onAuthStateChanged(authService, (user) => {
-			if (user) {
-				setIsLoggedIn(user);
-			} else {
-				setIsLoggedIn(false);
-			}
-			setInit(true);
+			setTimeout(() => {
+				if (user) {
+					setIsLoggedIn(user);
+				} else {
+					setIsLoggedIn(false);
+				}
+				window.KTApp.hidePageLoading();
+				setInit(true);
+			}, 500);
 		});
+		setTimeout(() => {
+			window.KTApp.showPageLoading();
+		}, 200);
 	}, []);
 	return (
 		<div className='app-page flex-column flex-column-fluid'>
-			{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'initializing ... '}
+			{init ? (
+				<AppRouter isLoggedIn={isLoggedIn} />
+			) : (
+				<div class='page-loader flex-column' style={{ backgroundColor: '#F8F2EC' }}>
+					<span class='spinner-border text-primary' role='status'></span>
+					<span class='text-gray-800 fs-6 fw-semibold mt-5'>Loading...</span>
+				</div>
+			)}
 			{/* <footer>&copy; {new Date().getFullYear()} Review App</footer> */}
 		</div>
 	);
